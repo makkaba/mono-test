554d258c858ba1c1ed70b5562bbdf66d4cfc0dac
makkaba <drifterz303@naver.com>
2016-05-26 17:43:56 +0900

    Initial Commit

---
+++ mono-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mono-test
+//
+//  Created by Sunyoung on 2016. 5. 26..
+//  Copyright © 2016년 kiwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
